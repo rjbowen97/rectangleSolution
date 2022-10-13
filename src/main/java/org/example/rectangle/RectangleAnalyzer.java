54e7874c016aedd35a6a3e59239b006011bfeb8a
rjbowen <rjbowen97@gmail.com>
2022-10-13 16:36:51 -0400

    Created a RectangleAnalyzer class with a static method to check for intersecting Rectangles

---
+++ src/main/java/org/example/rectangle/RectangleAnalyzer.java
@@ -0,0 +1,11 @@
+package org.example.rectangle;
+
+public class RectangleAnalyzer {
+
+    public static boolean checkRectanglesForIntersection(Rectangle rectangle1, Rectangle rectangle2) {
+
+        return false;
+    }
+
+
+}
