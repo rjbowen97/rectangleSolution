322e712435689e8c4e792a6664f86dc29f25ac3d
rjbowen <rjbowen97@gmail.com>
2022-10-14 13:29:55 -0400

    Added documentation for Rectangle.java

--- src/main/java/org/example/rectangle/Rectangle.java
+++ src/main/java/org/example/rectangle/Rectangle.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+/**
+ * Represents a rectangle in cartesian coordinate space. (Has four corners with edges connecting said corners at 90
+ * degree angles.)
+ */
 public class Rectangle {
     private Point bottomLeftPoint;
     private Point topRightPoint;
@@ -18,12 +22,37 @@ public class Rectangle {
     private Edge bottomEdge;
     private Edge leftEdge;
 
+    /**
+     * Checks if the given points form a valid rectangle and if so, sets the points of this rectangle and
+     * calculates the edges connecting the points of the rectangle.
+     *
+     * @param points A list of 4 cartesian coordinate pairs representing a rectangle.
+     * @throws RectangleException Thrown if the 4 cartesian coordinate pairs do not represent a rectangle.
+     */
     public Rectangle(List<Point> points) throws RectangleException {
         validateInputPoints(points);
         this.setPoints(points);
         this.setEdges();
     }
 
+    /**
+     * Sorts the points based on their distance from the origin of the cartesian coordinate system (0,0). Then,
+     * uses the indices of the sorted points to assign the points to their logical positions in the rectangle.
+     * This allows for easier intersection, containment, and adjacency calculations.
+     *
+     * @param points The list of points to represent this rectangle.
+     */
+    private void setPoints(List<Point> points) {
+        points.sort(Point::compareTo);
+        this.bottomLeftPoint = points.get(0);
+        this.topRightPoint = points.get(3);
+        this.topLeftPoint = new Point(bottomLeftPoint.getX(), topRightPoint.getY());
+        this.bottomRightPoint = new Point(topRightPoint.getX(), bottomLeftPoint.getY());
+    }
+
+    /**
+     * Calculates and sets the edges of the rectangle using the points of the rectangle.
+     */
     private void setEdges() {
         this.topEdge = new Edge(topLeftPoint, topRightPoint);
         this.rightEdge = new Edge(bottomRightPoint, topRightPoint);
@@ -31,6 +60,12 @@ public class Rectangle {
         this.leftEdge = new Edge(bottomLeftPoint, topLeftPoint);
     }
 
+    /**
+     * Checks if there are 4 input points and that the 4 points are all unique.
+     *
+     * @param points The points to check for valid rectangle formation
+     * @throws RectangleException An exception indicating why the points do not form a valid rectangle
+     */
     private void validateInputPoints(List<Point> points) throws RectangleException {
         boolean correctNumberOfPoints = points.size() == 4;
         if (!correctNumberOfPoints) {
@@ -44,39 +79,17 @@ public class Rectangle {
         }
     }
 
+    /**
+     * Checks if a given point is contained by this rectangle.
+     *
+     * @param point The point to be checked for containment by this rectangle
+     * @return boolean indicating if the point is contained
+     */
     public boolean contains(Point point) {
         boolean containsPointInXDirection = point.getX() > getLowerXBound() && point.getX() < getUpperXBound();
         boolean containsPointInYDirection = point.getY() > getLowerYBound() && point.getY() < getUpperYBound();
 
         return containsPointInXDirection && containsPointInYDirection;
-    }
-
-    public ArrayList<Point> getPoints() {
-        ArrayList<Point> points = new ArrayList<>();
-        points.add(bottomLeftPoint);
-        points.add(topRightPoint);
-        points.add(topLeftPoint);
-        points.add(bottomRightPoint);
-
-        return points;
-    }
-
-    private void setPoints(List<Point> points) {
-        points.sort(Point::compareTo);
-        this.bottomLeftPoint = points.get(0);
-        this.topRightPoint = points.get(3);
-        this.topLeftPoint = new Point(bottomLeftPoint.getX(), topRightPoint.getY());
-        this.bottomRightPoint = new Point(topRightPoint.getX(), bottomLeftPoint.getY());
-    }
-
-    public ArrayList<Edge> getEdges() {
-        ArrayList<Edge> edges = new ArrayList<>();
-        edges.add(topEdge);
-        edges.add(rightEdge);
-        edges.add(bottomEdge);
-        edges.add(leftEdge);
-
-        return edges;
     }
 
     private int getLowerXBound() {
@@ -93,6 +106,36 @@ public class Rectangle {
 
     private int getUpperYBound() {
         return this.topRightPoint.getY();
+    }
+
+    /**
+     * Gets the points representing this rectangle.
+     *
+     * @return The points representing this rectangle.
+     */
+    public ArrayList<Point> getPoints() {
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(bottomLeftPoint);
+        points.add(topRightPoint);
+        points.add(topLeftPoint);
+        points.add(bottomRightPoint);
+
+        return points;
+    }
+
+    /**
+     * Gets the edges connecting the points of this rectangle.
+     *
+     * @return The edges connecting the points of this rectangle.
+     */
+    public ArrayList<Edge> getEdges() {
+        ArrayList<Edge> edges = new ArrayList<>();
+        edges.add(topEdge);
+        edges.add(rightEdge);
+        edges.add(bottomEdge);
+        edges.add(leftEdge);
+
+        return edges;
     }
 
     @Override
@@ -141,6 +184,9 @@ public class Rectangle {
         return leftEdge;
     }
 
+    /**
+     * An exception representing a validation error when checking for a valid rectangle
+     */
     public static class RectangleException extends Exception {
         public RectangleException(String message) {
             super(message);
