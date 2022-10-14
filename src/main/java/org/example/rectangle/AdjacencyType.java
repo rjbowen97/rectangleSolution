be5d9363f6125e8770495c1cd9b0cf7ca7307984
rjbowen <rjbowen97@gmail.com>
2022-10-14 13:20:15 -0400

    Added documentation for AdjacencyType

--- src/main/java/org/example/rectangle/AdjacencyType.java
+++ src/main/java/org/example/rectangle/AdjacencyType.java
@@ -1,5 +1,12 @@
 package org.example.rectangle;
 
+/**
+ * Represents the type of adjacency between two rectangles.
+ * SUB_LINE: The edge of a rectangle is entirely contained within the edge of another rectangle.
+ * PROPER: The edge of a rectangle perfectly overlaps with the edge of another rectangle.
+ * PARTIAL: The edge of a rectangle partially overlaps with the edge of another rectangle.
+ * NONE: The edge of a rectangle does not overlap with the edge of another rectangle.
+ */
 public enum AdjacencyType {
     SUB_LINE, PROPER, PARTIAL, NONE
 }
