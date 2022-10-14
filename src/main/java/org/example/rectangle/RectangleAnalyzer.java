a17a59c526099f611f3a6e5622b8c5998d297115
rjbowen <rjbowen97@gmail.com>
2022-10-14 13:34:02 -0400

    Added documentation for RectangleAnalyzer.java.

--- src/main/java/org/example/rectangle/RectangleAnalyzer.java
+++ src/main/java/org/example/rectangle/RectangleAnalyzer.java
@@ -9,7 +9,13 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class RectangleAnalyzer {
-
+    /**
+     * Using the edges of the two input rectangles, this function checks if any of the edges intersect and, if they do,
+     * returns the points representing said intersections.
+     * @param primaryRectangle
+     * @param otherRectangle
+     * @return The points at which the two rectangles intersect, or an empty list if no intersections are present.
+     */
     public static ArrayList<Point> getRectangleIntersectionPoints(Rectangle primaryRectangle, Rectangle otherRectangle) {
 
         ArrayList<Edge> primaryRectangleEdges = primaryRectangle.getEdges();
@@ -28,6 +34,12 @@ public class RectangleAnalyzer {
         return intersections;
     }
 
+    /**
+     * Checks if the primaryRectangle entirely contains the otherRectangle using the points of the otherRectangle.
+     * @param primaryRectangle
+     * @param otherRectangle
+     * @return boolean indicating containment of the otherRectangle by the primaryRectangle.
+     */
     public static boolean doesRectangleContainOtherRectangle(Rectangle primaryRectangle, Rectangle otherRectangle) {
         AtomicInteger pointsContainedByPrimaryRectangle = new AtomicInteger();
 
@@ -40,6 +52,13 @@ public class RectangleAnalyzer {
         return pointsContainedByPrimaryRectangle.get() == 4;
     }
 
+    /**
+     * Checks if the edges of two rectangles share any adjacencies with one another.
+     * See {@link AdjacencyType} for the definitions of adjacencies.
+     * @param primaryRectangle
+     * @param otherRectangle
+     * @return The {@link AdjacencyType} shared between the two rectangles.
+     */
     public static HashSet<AdjacencyType> getRectangleAdjacencies(Rectangle primaryRectangle, Rectangle otherRectangle) {
         ArrayList<Edge> primaryRectangleEdges = primaryRectangle.getEdges();
         ArrayList<Edge> otherRectangleEdges = otherRectangle.getEdges();
